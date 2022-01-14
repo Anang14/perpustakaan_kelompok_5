6cb9a71262a8336f96f6a2386e478b84e5974d13
rais70 <muhaminrais28@gmail.com>
2022-01-14 06:20:52 -0800

    perubahan export

--- resources/views/laporan/index.blade.php
+++ resources/views/laporan/index.blade.php
@@ -16,16 +16,16 @@
                         <div class="col-md-6">
                             <div class="card-text mb-2"><i class="ni ni-book-bookmark"></i> Buku</div>
                             <section class="buku">
-                                <a class="btn btn-danger " href="{{ route('buku.pdf') }}" target="_blank"><i class="ni ni-cloud-download-95"></i> Export Pdf</a>
+                                <a class="btn btn-danger " href="{{ route('buku.pdf') }}" target="_blank"><i class="ni ni-cloud-download-95"></i> Download Pdf</a>
                                 <a class="btn btn-success" href="{{ route('buku.excel') }}" target="_blank">
-                                 <i class="ni ni-cloud-download-95"></i> Export Excel</a>
+                                 <i class="ni ni-cloud-download-95"></i> Download Excel</a>
                             </section>
                           </div>
                        <div class="col-md-6 text-right">
                             <div class="card-text mb-2 ">Transaksi <i class="ni ni-ruler-pencil"></i></div>
                                 <section class="transaksi">
-                                    <a class="btn btn-danger " href="{{ route('transaksi.pdf') }}" target="_blank"><i class="ni ni-cloud-download-95"></i> Export Pdf</a>
-                                    <a class="btn btn-success " href="{{ route('transaksi.excel') }}"><i class="ni ni-cloud-download-95"></i>  Export Excel</a>
+                                    <a class="btn btn-danger " href="{{ route('transaksi.pdf') }}" target="_blank"><i class="ni ni-cloud-download-95"></i> Download Pdf</a>
+                                    <a class="btn btn-success " href="{{ route('transaksi.excel') }}"><i class="ni ni-cloud-download-95"></i>  Download Excel</a>
                                 </section>
                             </div>
                        </div>
